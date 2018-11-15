7e8d96f06eb4e892df365c1312a6781ac09ea4f4
Davide <deddolo@gmail.com>
2018-11-15 13:23:21 -0800

    smartsheet import

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,19 @@
 from flask import Flask
 from flask import Response
+import smartsheet
 #init app
 app = Flask(__name__)
+ss_client = smartsheet.Smartsheet('l07xoamqp4rg0v9helj4talqy2')
 
+print("I'm starting...")
 @app.route("/")
 def hello():
-    return "Hello World!"
+    user_profile = ss_client.Users.get_current_user()
+    return json.dumps(user_profile)
 
 @app.route("/test", methods=['GET','POST'])
 def response():
     return Response('',status=200)
+
+@app.route("/createwebhook")
+def create_webhook():
